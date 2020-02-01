296f14839f4abb81c7e5ffb58c7c9615a2709674
Chris Rodriguez <chrismail627@gmail.com>
2020-01-31 21:46:15 -0800

    Initial Commit

---
+++ NoCaption/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NoCaption
+//
+//  Created by Chris Rodriguez on 1/31/20.
+//  Copyright © 2020 Chris. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
